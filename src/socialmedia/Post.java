5919ef786e5543dcd898e410f1dce2ca23544483
mafarl <108734570+mafarl@users.noreply.github.com>
2023-02-27 15:34:51 +0000

    Update Post.java

--- src/socialmedia/Post.java
+++ src/socialmedia/Post.java
@@ -1,39 +1,39 @@
-import java.*;
+import java.util.*;
 
 class Post{
 	
-	// empty hash map which will be passed to the post constructor
-	HashMap<String, ArrayList<Integer>> storage = new HashMap<String, ArrayList<Integer>>();
+	// attributes
+	private int numIdentifier;
+	private String message;
+	private int ID;
+	private int pointerToOriginal;
 	
-	// constructor
-	// original
-	// should have an array to store all the comments and endorsements
-	public Post(int numIdentifier,String message,int accountID, HashMap storage){
-		this.numIdentifier = numIdentifier;
-		this.message = message;
-		this.accountID = accountID;
-		this.storage = storage;
-		// endorsements - the no. endorsements the post has
-		// comments - the no. comments the post has
-		storage.put("comments", new ArrayList<Integer>());
-		storage.put("endorsements", new ArrayList<Integer>());
-		/*
-		arrayOfPosts = {endorsements: [id],
-						comments: [id]} */
+	// getter methods
+	public int getNumIdentifier(){
+		return numIdentifier;
+	}
+	public String getMessage(){
+		return message;
+	}
+	public int getPointerToOriginal(){
+		return pointerToOriginal;
 	}
 	
-	// comment
+	
+	// constructor
+	// original and comment
 	// should have an array to store all the comments and endorsements
-	public Post(int numIdentifier,String message, int pointerToOriginal, HashMap storage){
+	public Post(int numIdentifier,String message,int pointerToOriginal){
 		this.numIdentifier = numIdentifier;
 		this.message = message;
 		this.pointerToOriginal = pointerToOriginal;
-		this.storage = storage;
+		HashMap<String, ArrayList<Integer>> storage = new HashMap<String, ArrayList<Integer>>();
 		// endorsements - the no. endorsements the post has
 		// comments - the no. comments the post has
 		storage.put("comments", new ArrayList<Integer>());
 		storage.put("endorsements", new ArrayList<Integer>());
 	}
+	
 	
 	// endorsement
 	public Post(int numIdentifier,int pointerToOriginal){
