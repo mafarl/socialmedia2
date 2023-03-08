752495bb73ce979a597999e74a985933e6069ebb
mafarl <108734570+mafarl@users.noreply.github.com>
2023-03-08 16:07:35 +0000

    Update Post.java

--- src/socialmedia/Post.java
+++ src/socialmedia/Post.java
@@ -23,9 +23,36 @@ public class Post{
 		return storage;
 	}
 	
+	//setter methods
+	public void addToPostStorageComment(int idPost){
+		ArrayList<Integer> value = new ArrayList<>();
+		value = storage.get("comments");
+		value.add(idPost);
+		storage.put("comments", value);
+		
+	}
+	public void addToPostStorageEndors(int idPost){
+		ArrayList<Integer> value = new ArrayList<>();
+		value = storage.get("endorsements");
+		value.add(idPost);
+		storage.put("endorsements", value);
+		
+	}
+	
+	
 	// constructor
-	// original and comment
+	// original
 	// should have an array to store all the comments and endorsements
+	public Post(int numIdentifier,String message){
+		this.numIdentifier = numIdentifier;
+		this.message = message;
+		storage = new HashMap<String, ArrayList<Integer>>();
+		// endorsements - the no. endorsements the post has
+		// comments - the no. comments the post has
+		storage.put("comments", new ArrayList<Integer>());
+		storage.put("endorsements", new ArrayList<Integer>());
+	}
+	//comment
 	public Post(int numIdentifier,String message,int pointerToOriginal){
 		this.numIdentifier = numIdentifier;
 		this.message = message;
